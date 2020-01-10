f650b75053ccef1b2bf45d84566da7f268154ef7
Sean Batzel <romulus108@protonmail.com>
2020-01-10 18:19:33 -0500

    Crypto library skeleton.

---
+++ core/src/crypto/crypto.rs
@@ -0,0 +1,13 @@
+fn generate_server_key() {}
+
+fn generate_server_key_shares() {}
+
+fn generate_document_key() {}
+
+fn encrypt_document_key() {}
+
+fn decrypt_document_key() {}
+
+fn generate_signature() {}
+
+fn verify_signature() {}
