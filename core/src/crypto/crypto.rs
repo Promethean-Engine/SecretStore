b659bc0a8e8e59a480a6cee8ac77d0a11ee81685
Jonathan Koller <jkoller@pop-os.localdomain>
2020-01-11 15:40:30 -0500

    key gen steps clarified; crypto.rs not a huge mess anymore

--- core/src/crypto/crypto.rs
+++ core/src/crypto/crypto.rs
@@ -1,6 +1,6 @@
 use parity_crypto::publickey::{Public, Secret, Signature, Random, Generator, ec_math_utils, KeyPair, recover, verify_public};
 
-use crate::types::EncryptedDocumentKey;
+use crate::types::{EncryptedDocumentKey, Error};
 
 #[derive(Clone)]
 struct KeyGenerationArtifacts{
@@ -19,112 +19,45 @@ struct ZeroGenerationArtifacts{
 
 pub trait Signable {
     fn sign(&self, public_key: Public) -> Signature {
-
+        let mut sig: Signature;
+        sig
     }
 
     fn verify(&self, public_key: Public) -> bool {
-
+        let mut verified:bool;
+        verified
     }
 }
 
 pub fn generate_server_key() -> (Public,Secret) {
+    // data gathered during initialization: derived_point (generate_random_scalar()) and id_numbers:Vec<_>
+    // data generated during key dist: polynoms1 (prepare_polynoms1()) and secrets1:Vec<_> (compute_polynom())
+    // verification: polynoms2, secrets2, publics:Vec<_>(public_values_generation())
+    // verification: keys_verification()
+    
+    // data gathered during key gen: public_shares:Vec<_> (compute_public_share) and secret_shares:Vec<_>(compute_secret_shares())
 
+    // joint public key as result of dist key gen: compute_joint_public(public_shares.iter()).unwrap()
 }
-// fns prepare_polynoms1 and run_key_gen (554 and 569) in math
-
-
-// fn prepare_polynoms1 (554): 
-
-/*
-1. generate 2d mut Vector of random polynomials, called polynoms1 :
-
-let mut polynoms1: Vec<_> = (0..n).map(|_|, generate_random_polynom(t).unwrap()).collect(); 
-
-
-2. if secret_required, do 3 and 4: 
-3. for all polynoms,
-
-a) secret_coeff1 = generated and unwrapped random scalar :
-    (gen_rand_scalar is in math line 46)
-let secret_coeff1 = generate_random_scalar().unwrap();  
-
-b) unwrap Secret's (which is struct type) inner:H256 then subtract secret_coeff1 : 
-secret_required.sub(&secret_coeff!).unwrap();
-
-
-4. end of loop, put secret_required in polynoms1
-
-poynoms1[n-1][0] = secret_required;
-
-5. return polynoms1, type Vec<Vec<Secret>> 
-*/
-
-// fn run_key_gen, returns KeyGenerationArtifacts as defined at the top of this file:
-
-/* 
-fn run_key_generation(
-    t:usize,n:usize, 
-    id_numbers: Option<vec<Secret>>, 
-    secret_required: Option<Secret>
-) -> KeyGenerationArtifacts
-
-    // data gathered during init 
-let derived_point = Random.generate().unwrap().public().clone();
-let id_numbers: Vec<_> = match id_numbers{
-    Some(id_numbers) => id_numbers,
-    None => (0..n).map(|_| generate_random_scalar().unwrap()).collect), 
-};
-
-    //data generated during key distribution
-let polynoms1 = prepare_polynoms1(t,n,secret_required);
-let secrets1: Vec<_> = (0..n).map(|i| (0..n).map(|j| compute_polynom(&polynoms1[i], &id_numbers[j]).unwrap()).collect::<Vec<_>>()).collect();
-
-// following data is used only on verification step
-let polynoms2: Vec<_> = (0..n).map(|_| generate_random_polynom(t).unwrap()).collect();
-let secrets2: Vec<_> = (0..n).map(|i| (0..n).map(|j| compute_polynom(&polynoms2[i], &id_numbers[j]).unwrap()).collect::<Vec<_>>()).collect();
-let publics: Vec<_> = (0..n).map(|i| public_values_generation(t, &derived_point, &polynoms1[i], &polynoms2[i]).unwrap()).collect();
-
-// keys verification
-(0..n).for_each(|i| {
-    (0..n)
-        .filter(|&j| i != j)
-        .for_each(|j| {
-            assert!(keys_verification(t, &derived_point, &id_numbers[i], &secrets1[j][i], &secrets2[j][i], &publics[j]).unwrap());
-        })
-});
-
-// data, generated during keys generation
-let public_shares: Vec<_> = (0..n).map(|i| compute_public_share(&polynoms1[i][0]).unwrap()).collect();
-let secret_shares: Vec<_> = (0..n).map(|i| compute_secret_share(secrets1.iter().map(|s| &s[i])).unwrap()).collect();
-
-// joint public key, as a result of DKG
-let joint_public = compute_joint_public(public_shares.iter()).unwrap();
-
-// return 
-KeyGenerationArtifacts {
-    id_numbers: id_numbers,
-    polynoms1: polynoms1,
-    secrets1: secrets1,
-    public_shares: public_shares,
-    secret_shares: secret_shares,
-    joint_public: joint_public,
-}
-*/
 
 pub fn generate_server_key_shares() -> Vec<Secret> {
-
+    let mut server_key_shares: Vec<Secret>;
+    server_key_shares
 }
 
 pub fn generate_document_key() -> (Public,Secret) {
-
+    let doc_key:(Public,Secret);
+    doc_key
 }
 
 // TODO Return type should be the encrypted document key representation
-pub fn encrypt_document_key(document_secret_key: Secret, server_secret_key: Secret) -> EncryptedSecretKey {
-
+pub fn encrypt_document_key(document_secret_key: Secret, server_secret_key: Secret) -> EncryptedDocumentKey { //EncryptedSecretKey
+    let enc_secretkey: EncryptedDocumentKey;
+    enc_secretkey
 }
 
 // TODO Argument should be the encrypted document key representation
-pub fn decrypt_document_key(key: EncryptedSecretKey) -> Secret {
-
+pub fn decrypt_document_key(key: EncryptedDocumentKey) -> Secret {//EncryptedSecretKey
+    let secret:Secret;
+    secret
 }
