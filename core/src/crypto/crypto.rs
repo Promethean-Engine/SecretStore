b7baa8208765f4f5849bb347fcb14b311ab39735
Sean Batzel <romulus108@protonmail.com>
2020-01-12 11:47:34 -0500

    In what can only be termed a miracle, crypto.rs compiles.

--- core/src/crypto/crypto.rs
+++ core/src/crypto/crypto.rs
@@ -1,7 +1,6 @@
 use parity_crypto::publickey::{Public, Secret, Signature, Random, Generator, ec_math_utils, KeyPair, recover, verify_public};
 
 use crate::types::{EncryptedDocumentKey, Error};
-use bytes::Bytes;
 
 use super::math::*;
 
@@ -12,54 +11,104 @@ pub fn generate_server_key(
 { 
     // dummy data generated during initialization
     let derived_point = Random.generate().unwrap().public().clone();
-	let id_numbers: Vec<_> = match id_numbers {
-		Some(id_numbers) => id_numbers,
-		None => (0..n).map(|_| generate_random_scalar().unwrap()).collect(),
+    let id_numbers: Vec<_> = match id_numbers {
+        Some(id_numbers) => id_numbers,
+        None => (0..n).map(|_| generate_random_scalar().unwrap()).collect(),
     };
-    
+
     // data from distribution
     let polynoms1 = prepare_polynoms1(t,n,secret_required);
     let secrets1: Vec<_> = (0..n)
-    .map(|i| {
-        (0..n)
-            .map(|j| compute_polynom(&polynoms1[i], &id_numbers[j]).unwrap())
-            .collect::<Vec<_>>()
-    }).collect();
+        .map(|i| {
+            (0..n)
+                .map(|j| compute_polynom(&polynoms1[i], &id_numbers[j]).unwrap())
+                .collect::<Vec<_>>()
+        }).collect();
 
     //data for verification (dummy data?)
     let polynoms2: Vec<_> = (0..n)
-		.map(|_| generate_random_polynom(t).unwrap())
-		.collect();
-	let secrets2: Vec<_> = (0..n)
-		.map(|i| {
-			(0..n)
-				.map(|j| compute_polynom(&polynoms2[i], &id_numbers[j]).unwrap())
-				.collect::<Vec<_>>()
-		})
-		.collect();
-	let publics: Vec<_> = (0..n)
-		.map(|i| {
-			public_values_generation(t, &derived_point, &polynoms1[i], &polynoms2[i]).unwrap()
-		})
+        .map(|_| generate_random_polynom(t).unwrap())
         .collect();
+    let secrets2: Vec<_> = (0..n)
+        .map(|i| {
+            (0..n)
+                .map(|j| compute_polynom(&polynoms2[i], &id_numbers[j]).unwrap())
+                .collect::<Vec<_>>()
+        })
+    .collect();
+    let publics: Vec<_> = (0..n)
+        .map(|i| {
+            public_values_generation(t, &derived_point, &polynoms1[i], &polynoms2[i]).unwrap()
+        })
+    .collect();
 
     // data from key generation 
-	let public_shares: Vec<_> = (0..n)
-		.map(|i| compute_public_share(&polynoms1[i][0]).unwrap())
-		.collect();
-	let secret_shares: Vec<_> = (0..n)
-		.map(|i| compute_secret_share(secrets1.iter().map(|s| &s[i])).unwrap())
-		.collect();
+    let public_shares: Vec<_> = (0..n)
+        .map(|i| compute_public_share(&polynoms1[i][0]).unwrap())
+        .collect();
+    let secret_shares: Vec<_> = (0..n)
+        .map(|i| compute_secret_share(secrets1.iter().map(|s| &s[i])).unwrap())
+        .collect();
 
-	// joint public key, as a result of DKG
-	let joint_public = compute_joint_public(public_shares.iter()).unwrap();
+    // joint public key, as a result of DKG
+    let joint_public = compute_joint_public(public_shares.iter()).unwrap();
 
     // generate (Vec<Public>,Vec<Secret>)
     (public_shares,secret_shares)
 }
 
-pub fn generate_document_key() -> (Public,Secret) {
+pub fn generate_document_key(
+    t:usize, n:usize,
+    id_numbers: Option<Vec<Secret>>,
+    secret_required: Option<Secret>) -> (Vec<Public>,Vec<Secret>) {
+    // dummy data generated during initialization
+    let derived_point = Random.generate().unwrap().public().clone();
+    let id_numbers: Vec<_> = match id_numbers {
+        Some(id_numbers) => id_numbers,
+        None => (0..n).map(|_| generate_random_scalar().unwrap()).collect(),
+    };
+
+    // data from distribution
+    let polynoms1 = prepare_polynoms1(t,n,secret_required);
+    let secrets1: Vec<_> = (0..n)
+        .map(|i| {
+            (0..n)
+                .map(|j| compute_polynom(&polynoms1[i], &id_numbers[j]).unwrap())
+                .collect::<Vec<_>>()
+        }).collect();
+
+    //data for verification (dummy data?)
+    let polynoms2: Vec<_> = (0..n)
+        .map(|_| generate_random_polynom(t).unwrap())
+        .collect();
+    let secrets2: Vec<_> = (0..n)
+        .map(|i| {
+            (0..n)
+                .map(|j| compute_polynom(&polynoms2[i], &id_numbers[j]).unwrap())
+                .collect::<Vec<_>>()
+        })
+    .collect();
+    let publics: Vec<_> = (0..n)
+        .map(|i| {
+            public_values_generation(t, &derived_point, &polynoms1[i], &polynoms2[i]).unwrap()
+        })
+    .collect();
+
+    // data from key generation 
+    let public_shares: Vec<_> = (0..n)
+        .map(|i| compute_public_share(&polynoms1[i][0]).unwrap())
+        .collect();
+    let secret_shares: Vec<_> = (0..n)
+        .map(|i| compute_secret_share(secrets1.iter().map(|s| &s[i])).unwrap())
+        .collect();
+
+    // joint public key, as a result of DKG
+    let joint_public = compute_joint_public(public_shares.iter()).unwrap();
+
+    // generate (Vec<Public>,Vec<Secret>)
+    (public_shares,secret_shares)
 }
+
 
 pub fn encrypt_document_key(document_secret_key: Public, joint_public: Public) -> EncryptedDocumentKey {
     key_adapter(super::math::encrypt_secret(&joint_public, &document_secret_key).unwrap())
@@ -70,11 +119,11 @@ pub fn decrypt_document_key(
     access_key: &Secret,
     encrypted_point: &Public,
     joint_shadow_point: &Public) -> Public {
-        super::math::decrypt_with_joint_shadow(
-            threshold,
-            &access_key,
-            &encrypted_point,
-            &joint_shadow_point
+    super::math::decrypt_with_joint_shadow(
+        threshold,
+        &access_key,
+        &encrypted_point,
+        &joint_shadow_point
         ).unwrap()
 }
 
