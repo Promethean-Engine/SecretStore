f796e95e59404cef2cb0a59680448565b8f717b1
Sean Batzel <romulus108@protonmail.com>
2020-01-14 16:12:58 -0500

    Started structural changes.

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,12 @@
 
 extern crate parity_crypto as crypto;
 extern crate parity_bytes as bytes;
+
 use codec::{Encode, Decode};
+
 mod database;
 mod types;
+mod net;
 
 // use database::PermissionDatabase;
 
